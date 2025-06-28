741633fcb2e2f1305579ea27f0d413cd4041995e
Nikita22007 <game.nikita22007@yandex.ru>
2025-06-28 22:20:19 +0300

    use constansts in filter

--- jwt-module/src/main/java/com/yazikochesalna/common/filter/JwtFilter.java
+++ jwt-module/src/main/java/com/yazikochesalna/common/filter/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+    private static final String TOKEN_PREFIX = "Bearer ";
     private final JwtService jwtService;
 
     @Override
@@ -27,14 +29,14 @@ public class JwtFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        String authHeader = request.getHeader("Authorization");
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || !authHeader.startsWith(TOKEN_PREFIX)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        String token = authHeader.substring(7);
+        String token = authHeader.substring(TOKEN_PREFIX.length()); //cut prefix
 
         if (SecurityContextHolder.getContext().getAuthentication() == null) {
             if (jwtService.isValidAccess(token)) {
