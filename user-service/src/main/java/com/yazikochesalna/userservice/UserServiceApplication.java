1f38b2a17debd95bfb3ba294bce9bbc1bb5611bd
Nikita22007 <game.nikita22007@yandex.ru>
2025-06-28 21:02:05 +0300

    add JWT to user-serivice

--- user-service/src/main/java/com/yazikochesalna/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/yazikochesalna/userservice/UserServiceApplication.java
@@ -3,7 +3,10 @@ package com.yazikochesalna.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = {
+        "com.yazikochesalna.userservice",
+        "com.yazikochesalna.common"
+})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
