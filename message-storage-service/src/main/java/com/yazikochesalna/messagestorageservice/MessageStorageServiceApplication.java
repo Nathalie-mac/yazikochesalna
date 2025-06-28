ff95c22b67fe089e4fe18d67812c2c2c32cc3504
Nikita22007 <game.nikita22007@yandex.ru>
2025-06-28 20:56:43 +0300

    add JWT to message-storege-service

--- message-storage-service/src/main/java/com/yazikochesalna/messagestorageservice/MessageStorageServiceApplication.java
+++ message-storage-service/src/main/java/com/yazikochesalna/messagestorageservice/MessageStorageServiceApplication.java
@@ -3,7 +3,10 @@ package com.yazikochesalna.messagestorageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = {
+        "com.yazikochesalna.messagestorageservice",
+        "com.yazikochesalna.common"
+})
 public class MessageStorageServiceApplication {
 
     public static void main(String[] args) {
