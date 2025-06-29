b323b88ac09a4d79b2883fbe27eb64a46f901bbb
Nikita22007 <game.nikita22007@yandex.ru>
2025-06-30 01:17:09 +0300

    Configure Swagger to use bearer authorization

--- chat-service/src/main/java/com/yazikochesalna/chatservice/controller/ChatsController.java
+++ chat-service/src/main/java/com/yazikochesalna/chatservice/controller/ChatsController.java
@@ -5,8 +5,13 @@ import com.yazikochesalna.chatservice.dto.createChat.CreateChatRequest;
 import com.yazikochesalna.chatservice.dto.createChat.CreateChatResponse;
 import com.yazikochesalna.chatservice.service.ChatService;
 import com.yazikochesalna.common.authentication.JwtAuthenticationToken;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +21,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/chats")
+@SecurityRequirement(name = "bearerAuth")
 public class ChatsController {
 
     private final ChatService chatService;
 
     @GetMapping({"/", ""})
+    @Operation(
+            summary = "Получить список чатов пользователя",
+            description = "Возвращает все чаты, в которых пользователь состоит"
+    )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Информация о чатах получена"),
-            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+            @ApiResponse(responseCode = "200",
+                    description = "Информация о чатах получена",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ChatListDto.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера", content = {@Content()})
     })
     public ResponseEntity<ChatListDto> getUserChats(){
         final long userId = ((JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication()).getUserId();
