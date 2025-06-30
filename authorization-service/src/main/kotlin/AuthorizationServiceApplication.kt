885f23b97fa8ff3002b92625daf4dc1999fa95ab
Nikita22007 <game.nikita22007@yandex.ru>
2025-06-30 15:06:30 +0300

    fix package name

--- authorization-service/src/main/kotlin/AuthorizationServiceApplication.kt
+++ authorization-service/src/main/kotlin/AuthorizationServiceApplication.kt
@@ -1,4 +1,4 @@
-package com.yazikochesaina.authorizationservice
+package com.yazikochesalna.authorizationservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
