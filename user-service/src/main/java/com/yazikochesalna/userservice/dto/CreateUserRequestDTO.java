3806cb95ce61ff30472f61f201bd8f30bda5d7c7
rubberPlant256 <ihrustal@yandex.ru>
2025-07-07 07:50:10 +0000

    user-service/get personal profile

---
+++ user-service/src/main/java/com/yazikochesalna/userservice/dto/CreateUserRequestDTO.java
@@ -0,0 +1,15 @@
+package com.yazikochesalna.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateUserRequestDTO {
+
+    private String username;
+}
