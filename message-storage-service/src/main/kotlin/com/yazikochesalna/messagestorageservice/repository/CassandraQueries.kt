6c8c9fb710594c1270c5876fe442fb43a1b18c33
nathalie_mac <natulka0506@gmail.com>
2025-07-08 20:30:01 +0300

    fix in cassandra queries

--- message-storage-service/src/main/kotlin/com/yazikochesalna/messagestorageservice/repository/CassandraQueries.kt
+++ message-storage-service/src/main/kotlin/com/yazikochesalna/messagestorageservice/repository/CassandraQueries.kt
@@ -3,18 +3,18 @@ package com.yazikochesalna.messagestorageservice.repository
 object CassandraQueries {
     const val CURSOR_MESSAGE_SELECT = """
         SELECT * FROM messages 
-        WHERE chat_id = ? AND message_id = ?
+        WHERE id = ?
     """
 
     const val BEFORE_CURSOR_SELECT = """
-        SELECT * FROM messages 
+        SELECT * FROM messages_by_chat 
         WHERE chat_id = ? AND send_time < ? 
         ORDER BY send_time DESC 
         LIMIT ?
     """
 
     const val AFTER_CURSOR_SELECT = """
-        SELECT * FROM messages 
+        SELECT * FROM messages_by_chat
         WHERE chat_id = ? AND send_time > ? 
         ORDER BY send_time ASC 
         LIMIT ?
