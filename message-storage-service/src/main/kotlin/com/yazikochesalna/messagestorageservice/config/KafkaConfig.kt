535d6cccc6259e492115759284529f9862a275fb
nathalie_mac <natulka0506@gmail.com>
2025-07-09 23:16:03 +0300

    pupupu

---
+++ message-storage-service/src/main/kotlin/com/yazikochesalna/messagestorageservice/config/KafkaConfig.kt
@@ -0,0 +1,11 @@
+package com.yazikochesalna.messagestorageservice.config
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class KafkaConfig(
+
+) {
+    // val kafkaProperties: KafkaProperties
+}
