6cedc440eadbc915d95f4140918b3bd21ebe3e2d
Ineptias <liza.antipa@gmail.com>
2025-07-10 09:00:40 +0000

    Messaging service/fix auth

--- messaging-service/src/main/java/com/yazikochesalna/messagingservice/config/WebSocketConfig.java
+++ messaging-service/src/main/java/com/yazikochesalna/messagingservice/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "/chat/connect")
+        registry.addHandler(chatWebSocketHandler, "/ws")
                 .addInterceptors(webSocketHandshakeInterceptor)
                 .setAllowedOriginPatterns(chatServiceProperties.getUrl(), frontendProperties.getUrl());
     }
