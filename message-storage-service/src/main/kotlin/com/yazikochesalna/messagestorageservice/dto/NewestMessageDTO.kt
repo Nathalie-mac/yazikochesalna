0c8d5ada9a2edfc9ff78ec84d9a3db7c2cd42263
nathalie_mac <natulka0506@gmail.com>
2025-07-16 13:27:27 +0300

    internal api for ChatService

---
+++ message-storage-service/src/main/kotlin/com/yazikochesalna/messagestorageservice/dto/NewestMessageDTO.kt
@@ -0,0 +1,11 @@
+package com.yazikochesalna.messagestorageservice.dto
+
+import lombok.Builder
+import lombok.Data
+
+@Builder
+@Data
+data class NewestMessageDTO(
+    var chatId: Long,
+    var messagesJsonFormatDTO: MessagesJsonFormatDTO?
+)
