eade97fad103393134f96b36222cb770dea01201
Nikita22007 <game.nikita22007@yandex.ru>
2025-07-16 22:12:28 +0300

    auth-service/fix for cotlin

--- authorization-service/src/main/kotlin/com/yazikochesalna/authorizationservice/AuthorizationServiceApplication.kt
+++ authorization-service/src/main/kotlin/com/yazikochesalna/authorizationservice/AuthorizationServiceApplication.kt
@@ -4,13 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = [
+@SpringBootApplication(
+    scanBasePackages = [
     "com.yazikochesalna.authorizationservice",
     "com.yazikochesalna.common",
     "org.springframework.security.crypto.password",
     "org.springframework.security.authentication",
 ],
-    exclude = { ErrorMvcAutoConfiguration.class}
+    exclude = [ErrorMvcAutoConfiguration::class],
 )
 open class AuthorizationServiceApplication
 
