6a1154988d6a8a143895165d39d9dc5720b524bb
Nikita22007 <game.nikita22007@yandex.ru>
2025-07-18 13:29:56 +0300

    chat-service/fix user added notify

--- chat-service/src/main/java/com/yazikochesalna/chatservice/model/ChatUser.java
+++ chat-service/src/main/java/com/yazikochesalna/chatservice/model/ChatUser.java
@@ -17,7 +17,8 @@ import java.util.UUID;
 public class ChatUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "id", nullable = false)
+    private Long recordId;
     @Column(name = "user_id", nullable = false)
     private Long userId;
     @Column(name = "last_read_message_id", nullable = true)
